dad932bf518548375b511edb8045ee8de738466a
Nick Jablonski <nick.jablonski@dominodatalab.com>
2024-04-21 20:25:41 -0700

    data refactor & top users graph
    Pushed from Domino: https://se-demo.domino.tech/workspace/nick_jablonski/quick-start?executionId=66216ec55f543d32745a4dc9

--- dash-cost-dashboard.py
+++ dash-cost-dashboard.py
@@ -13,6 +13,7 @@ import os
 import re
 import requests
 import plotly.graph_objs as go
+import plotly.express as px
 from collections import defaultdict
 from typing import Dict, List
 from datetime import datetime, timedelta
@@ -58,45 +59,15 @@ breakdown_to_param = {
 
 
 # For granular aggregations
-window_options = ["Last 60 days", "Last 30 days", "Last 14 days", "Last week", "Today"]
 window_to_param = {
-    "Last 60 days": "60d",
-    "Last 30 days": "30d",
-    "Last 14 days": "14d",
-    "Last week": "lastweek",
-    "Today": "today",
+    "30d": "Last 30 days",
+    "14d": "Last 14 days",
+    "lastweek": "Last week"
 }
-dropdown_options = []
-for key in window_to_param.keys():
-    option = {}
-    option['label'] = key
-    option['value'] = key
-    dropdown_options.append(option)
-window_choice = window_options[3]
-
-def format_datetime(dt_str: str) -> str:
-    datetime_object = datetime.strptime(dt_str, "%Y-%m-%dT%H:%M:%SZ")
-    return datetime_object.strftime("%Y/%m/%d, %H:%M:%S")
-
-def to_date(date_string: str) -> str:
-    """Converts minute-level date string to day level
-
-    ex:
-       to_date(2023-04-28T15:05:00Z) -> 2023-04-28
-    """
-    dt = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
-    return dt.strftime("%Y-%m-%d")
-
-
-def add_day(date: str, days: int) -> str:
-    dt = datetime.strptime(date, "%Y-%m-%d")
-    dt_new = dt + timedelta(days=days)
-    return dt_new.strftime("%Y-%m-%d")
-
 
 def get_aggregated_allocations(selection):
     params = {
-        "window": window_to_param[window_choice],
+        "window": selection,
         "aggregate": (
             "label:dominodatalab.com/workload-type,"
             "label:dominodatalab.com/project-id,"
@@ -105,7 +76,7 @@ def get_aggregated_allocations(selection):
             "label:dominodatalab.com/organization-name,"
             "label:dominodatalab.com/billing-tag,"
         ),
-        "accumulate": True,
+        "accumulate": False,
     }
 
     
@@ -118,90 +89,12 @@ def get_aggregated_allocations(selection):
 
     return list(filtered)
 
-
-
-def get_top_level_cost() -> Dict[str, float]:
-    params = {
-        "window": window_to_param[window_choice],
-        "accumulate": True,
-    }
-
-    res = requests.get(assets_url, params=params, headers=auth_header) 
-    
-    res.raise_for_status() 
-    data = res.json()["data"]
-    
-    accumulated_data = dict()
-
-    for cost_record in data:
-        cost_type = cost_record["type"]
-        accumulated_data[cost_type] = accumulated_data.get(cost_type, 0) + cost_record["totalCost"]
-
-    overAllCost = {cost_data: round(accumulated_data[cost_data], 2) for cost_data in accumulated_data}
-     
-    return overAllCost
-
-
-def get_daily_cost(selection) -> pd.DataFrame:
-    window = window_to_param[selection]
-    params = {
-        "window": window,
-        "aggregate": (
-            "label:dominodatalab.com/organization-name"
-        ),
-    }
-
-    res = requests.get(allocations_url, params=params, headers=auth_header) 
-    
-    res.raise_for_status() 
-    data = res.json()["data"]
-    
-    # May not have all historical days
-    alocs = [day for day in data if day]
-    
-    # Route returns data non-cumulatively. We make it cumulative by summing over the
-    # returned windows (could be days, hours, weeks etc)
-    daily_costs = defaultdict(dict)
-
-    cpu_costs = ["cpuCost", "cpuCostAdjustment"]
-    gpu_costs = ["gpuCost", "gpuCostAdjustment"]
-    storage_costs = ["pvCost", "pvCostAdjustment", "ramCost", "ramCostAdjustment"]
-
-    costs = {"CPU": cpu_costs, "GPU": gpu_costs, "Storage": storage_costs}
-
-    # Gets the overall cost per day
-    for aloc in alocs:
-        start = aloc["window"]["start"]
-        for cost_type, cost_keys in costs.items():
-            if cost_type not in daily_costs[start]:
-                daily_costs[start][cost_type] = 0.0
-            daily_costs[start][cost_type] += sum(aloc.get(cost_key,0) for cost_key in cost_keys)
-
-    # Cumulative sum over the daily costs
-    cumulative_daily_costs = pd.DataFrame(daily_costs).T.sort_index()
-
-    
-    cumulative_daily_costs["CPU"] = (round(cumulative_daily_costs["CPU"].cumsum(),2) if "CPU" in cumulative_daily_costs else 0)
-    cumulative_daily_costs["GPU"] = (round(cumulative_daily_costs["GPU"].cumsum(),2) if "GPU" in cumulative_daily_costs else 0)
-    cumulative_daily_costs["Storage"] = (round(cumulative_daily_costs["Storage"].cumsum(),2) if "Storage" in cumulative_daily_costs else 0)
-
-
-    # Unless we are looking at today granularity, rollup values to the day level
-    # (they are returned at the 5min level)
-    if window != "today":
-        cumulative_daily_costs.index = cumulative_daily_costs.index.map(to_date)
-        cumulative_daily_costs = cumulative_daily_costs.groupby(level=0).max()
-
-    return cumulative_daily_costs
-
-
-
 def get_execution_cost_table(aggregated_allocations: List) -> pd.DataFrame:
 
     exec_data = []
 
-    cpu_cost_key = ["cpuCost", "gpuCost"]
-    gpu_cost_key = ["cpuCostAdjustment", "gpuCostAdjustment"]
+    cpu_cost_key = ["cpuCost", "cpuCostAdjustment"]
+    gpu_cost_key = ["gpuCost", "gpuCostAdjustment"]
     storage_cost_keys = ["pvCost", "ramCost", "pvCostAdjustment", "ramCostAdjustment"]
 
     data = [costData for costData in aggregated_allocations if not costData["name"].startswith("__")]
@@ -227,16 +120,8 @@ def get_execution_cost_table(aggregated_allocations: List) -> pd.DataFrame:
             "TOTAL COST": f"${total_cost}"
         })
     execution_costs = pd.DataFrame(exec_data)
-    if all(windowKey in execution_costs for windowKey in ("START", "END")):
-        execution_costs["START"] = execution_costs["START"].apply(format_datetime)
-        execution_costs["END"] = execution_costs["END"].apply(format_datetime)
     
     return execution_costs
-
-df = get_daily_cost(window_choice)
-for column in df.columns:
-    df[column] = df[column].apply(lambda x: "${0:,.0f}".format(x))
-
 
 requests_pathname_prefix = '/{}/{}/r/notebookSession/{}/'.format(
     os.environ.get("DOMINO_PROJECT_OWNER"),
@@ -265,8 +150,8 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Dropdown(
                 id='time_span_select',
-                options = dropdown_options,
-                value = "Last 14 days",
+                options = window_to_param,
+                value = "lastweek",
                 clearable = False,
                 searchable = False
             ),
@@ -349,6 +234,13 @@ app.layout = html.Div([
         id='cumulative-daily-costs',
         config = {
             'displayModeBar': False
+        },
+        style={'margin-top': '40px'}
+    ),
+    dcc.Graph(
+        id='user_chart',
+        config = {
+            'displayModeBar': False
         }
     )
 ], className="container")
@@ -360,42 +252,50 @@ app.layout = html.Div([
       Output('storagecard', 'children'),
       Output('billing_select', 'options'),
       Output('project_select', 'options'),
-      Output('user_select', 'options')],
+      Output('user_select', 'options'),
+      Output('user_chart', 'figure')],
      [Input('time_span_select', 'value')]
 )
 def update(time_span):
-    updated_df = get_daily_cost(time_span)
-    compute_sum = '{0:.2f}'.format(updated_df.iloc[-1]['CPU'] + updated_df.iloc[-1]['GPU'])
-    storage_sum = '{0:.2f}'.format(updated_df.iloc[-1]['Storage'])
-    total_sum = '{0:.2f}'.format(float(compute_sum) + float(storage_sum))
-    
     allocations = get_aggregated_allocations(time_span)
     cost_table = get_execution_cost_table(allocations)
+    cost_table['TOTAL COST NUMERIC'] = cost_table['TOTAL COST'].str.replace('[\$,]', '', regex=True).astype(float)
+    cost_table['START'] = pd.to_datetime(cost_table['START'])
+    cost_table['FORMATTED START'] = cost_table['START'].dt.strftime('%B %-d')
+    
+    total_sum = round(cost_table['TOTAL COST'].replace('[\$,]', '', regex=True).astype(float).sum(), 2)
+    compute_sum = round(cost_table['COMPUTE COST'].replace('[\$,]', '', regex=True).astype(float).sum(), 2)
+    storage_sum = round(cost_table['STORAGE COST'].replace('[\$,]', '', regex=True).astype(float).sum(), 2)
     
     users = cost_table['USER'].unique().tolist()
     projects = cost_table['PROJECT NAME'].unique().tolist()
     billing_tags = cost_table['BILLING TAG'].unique().tolist()
     
-    for column in updated_df.columns:
-        updated_df[column] = updated_df[column].apply(lambda x: "${0:,.2f}".format(x))
-    
     cumulative_cost_graph = {
         'data': [
             go.Bar(
-                x=updated_df.index,
-                y=updated_df[column],
+                x=cost_table['FORMATTED START'],
+                y=cost_table[column],
                 name=column
-            ) for column in updated_df.columns
+            ) for column in ['CPU COST', 'GPU COST', 'STORAGE COST']
         ],
         'layout': go.Layout(
-            title='Cumulative Cost Over Time Span',
+            title='Daily Costs by Type',
             barmode='stack',
             yaxis_tickprefix = '$',
             yaxis_tickformat = ',.'
         )
     }
     
-    return cumulative_cost_graph, html.H4(f'${total_sum}'), html.H4(f'${compute_sum}'), html.H4(f'${storage_sum}'), billing_tags, projects, users
+    top_users = cost_table.groupby('USER')['TOTAL COST NUMERIC'].sum().nlargest(10).index
+    top_user_costs = cost_table[cost_table['USER'].isin(top_users)]
+    user_chart = px.histogram(top_user_costs, x='TOTAL COST NUMERIC', y='USER', orientation='h', 
+                              title='Total Cost by User', labels={'USER': 'User', 'TOTAL COST NUMERIC': 'Total Cost'},
+                              category_orders={'USER': top_user_costs.groupby('USER')['TOTAL COST NUMERIC'].sum().sort_values(ascending=False).index})
+    user_chart.update_layout(title_text='Top Users by Total Cost', title_x=0.5, xaxis_tickprefix = '$', xaxis_tickformat = ',.')
+    user_chart.update_xaxes(title_text="Total Cost")
+    
+    return cumulative_cost_graph, html.H4(f'${total_sum}'), html.H4(f'${compute_sum}'), html.H4(f'${storage_sum}'), billing_tags, projects, users, user_chart
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0',port=8888) # Domino hosts all apps at 0.0.0.0:8888
