bb59c5f73438966dd1c86d37f3951254fbea9234
ddl-olsonJD <olson.dimanche@dominodatalab.com>
2024-08-15 17:28:21 -0400

    some more refactors

--- dash_cost_dashboard.py
+++ dash_cost_dashboard.py
@@ -20,14 +20,13 @@ from pandas import (
 
 from domino_cost.domino_cost import (
     get_domino_namespace,
-    get_today_timestamp, get_time_delta
+    get_today_timestamp, get_time_delta, process_or_zero, clean_df, clean_values, distribute_cost, distribute_cloud_cost
 )
 from domino_cost.exceptions import TokenExpiredException
 from domino_cost.constants import (
     NO_TAG,
     window_to_param
 )
-
 
 #
 # def get_domino_namespace() -> str:
@@ -44,6 +43,7 @@ namespace = get_domino_namespace(api_host)
 cost_url = f"http://domino-cost.{namespace}:9000"
 auth_url = f"{api_proxy}/account/auth/service/authenticate"
 
+
 # class TokenExpiredException(Exception):
 #     pass
 
@@ -54,9 +54,11 @@ def get_token() -> str:
         raise TokenExpiredException("Your token has expired. Please redeploy your Domino Cost App.")
     return token
 
+
 auth_header = {
     'X-Authorization': get_token()
 }
+
 
 # NO_TAG = "No tag"
 #
@@ -76,48 +78,48 @@ auth_header = {
 #             days_to_use = int(time_span[:-1])
 #         return timedelta(days=days_to_use-1)
 
-def process_or_zero(func: Callable, posInt: int) -> int:
-    if posInt > 0:
-        return func
-    else:
-        return 0
+# def process_or_zero(func: Callable, posInt: int) -> int:
+#     if posInt > 0:
+#         return func
+#     else:
+#         return 0
 
-def distribute_cost(df: DataFrame) -> DataFrame:
-    """
-    distributes __unallocated__ cost for cleaner representation.
-    """
-    fiels_list = [ "CPU COST",	"GPU COST",	"COMPUTE COST",	"MEMORY COST",	"STORAGE COST",	"ALLOC COST"]
-    cost_unallocated = df[df["TYPE"].str.startswith("__")]
-    cost_allocated = df[~df["TYPE"].str.startswith("__")]
-    cost_allocated_total_sum = cost_allocated["ALLOC COST"].sum()
+# def distribute_cost(df: DataFrame) -> DataFrame:
+#     """
+#     distributes __unallocated__ cost for cleaner representation.
+#     """
+#     fiels_list = [ "CPU COST",	"GPU COST",	"COMPUTE COST",	"MEMORY COST",	"STORAGE COST",	"ALLOC COST"]
+#     cost_unallocated = df[df["TYPE"].str.startswith("__")]
+#     cost_allocated = df[~df["TYPE"].str.startswith("__")]
+#     cost_allocated_total_sum = cost_allocated["ALLOC COST"].sum()
+#
+#     for field in fiels_list:
+#         cost_allocated[field] = cost_allocated[field] + ((cost_allocated["ALLOC COST"] / cost_allocated_total_sum) * cost_unallocated[field].sum())
+#     return cost_allocated
 
-    for field in fiels_list:
-        cost_allocated[field] = cost_allocated[field] + ((cost_allocated["ALLOC COST"] / cost_allocated_total_sum) * cost_unallocated[field].sum())
-    return cost_allocated
+# def distribute_cloud_cost(df: DataFrame, cost: float) -> DataFrame:
+#     """
+#     distribute unaccounted cloud cost accross allocated executions.
+#     """
+#     accounted_cost = df["ALLOC COST"].sum()
+#     cloud_cost_unaccounted = process_or_zero(cost - accounted_cost, cost)
+#
+#     df['CLOUD COST'] = cloud_cost_unaccounted * (df['ALLOC COST'] / accounted_cost)
+#     df['TOTAL COST'] = df['ALLOC COST'] + df['CLOUD COST']
+#
+#     return df
 
-def distribute_cloud_cost(df: DataFrame, cost: float) -> DataFrame:
-    """
-    distribute unaccounted cloud cost accross allocated executions.
-    """
-    accounted_cost = df["ALLOC COST"].sum()
-    cloud_cost_unaccounted = process_or_zero(cost - accounted_cost, cost)
-   
-    df['CLOUD COST'] = cloud_cost_unaccounted * (df['ALLOC COST'] / accounted_cost)
-    df['TOTAL COST'] = df['ALLOC COST'] + df['CLOUD COST']
-    
-    return df
+# def clean_values(values_list: list) -> list:
+#     """
+#     remove "__unallocated__" from values'
+#     """
+#     return values_list[1:] if values_list[0].startswith("__") else values_list
 
-def clean_values(values_list: list) -> list:
-    """
-    remove "__unallocated__" from values'
-    """
-    return values_list[1:] if values_list[0].startswith("__") else values_list
-
-def clean_df(df: DataFrame, col: str) -> DataFrame:
-    """
-    remove "__unallocated__" records from dataframe for preview.
-    """
-    return df[~df[col].str.startswith("__")]
+# def clean_df(df: DataFrame, col: str) -> DataFrame:
+#     """
+#     remove "__unallocated__" records from dataframe for preview.
+#     """
+#     return df[~df[col].str.startswith("__")]
 
 def get_cloud_cost_sum(selection) -> float:
     cloud_cost_url = f"{cost_url}/cloudCost"
@@ -132,17 +134,18 @@ def get_cloud_cost_sum(selection) -> float:
     try:
         response = requests.request("GET", cloud_cost_url, headers=auth_header, params=parameters)
         response.raise_for_status()
-        
+
         cost_amortized = response.json()['data']['sets']
-        invoice_entity_id = list(cost_amortized[0]['cloudCosts'].keys())[0]   
-        
+        invoice_entity_id = list(cost_amortized[0]['cloudCosts'].keys())[0]
+
         for cost in cost_amortized:
             if cost['cloudCosts']:
                 cloud_cost_sum += cost['cloudCosts'][invoice_entity_id]['amortizedNetCost']['cost']
-    except Exception as e: # handle for users without cloudcost, or no data in cloudcost
+    except Exception as e:  # handle for users without cloudcost, or no data in cloudcost
         print(e)
-        
+
     return cloud_cost_sum
+
 
 def get_aggregated_allocations(selection: str) -> List:
     allocations_url = f"{cost_url}/allocation"
@@ -170,26 +173,25 @@ def get_aggregated_allocations(selection: str) -> List:
 
     return alloc_data
 
-def get_execution_cost_table(aggregated_allocations: List, cloud_cost: float) -> DataFrame:
 
+def get_execution_cost_table(aggregated_allocations: List, cloud_cost: float) -> DataFrame:
     exec_data = []
 
     storage_cost_keys = ["pvCost", "ramCost", "pvCostAdjustment", "ramCostAdjustment"]
 
     for costData in aggregated_allocations:
-
         workload_type, project_id, project_name, username, organization, billing_tag = costData["name"].split("/")
-        
+
         cpu_cost = costData["cpuCost"] + costData["cpuCostAdjustment"]
         gpu_cost = costData["gpuCost"] + costData["gpuCostAdjustment"]
         compute_cost = cpu_cost + gpu_cost
-        
+
         ram_cost = costData["ramCost"] + costData["ramCostAdjustment"]
-        
+
         alloc_total_cost = costData["totalCost"]
 
         storage_cost = alloc_total_cost - compute_cost
-        
+
         # Change __unallocated__ billing tag into "No Tag"
         billing_tag = billing_tag if billing_tag != '__unallocated__' else NO_TAG
 
@@ -217,6 +219,7 @@ def get_execution_cost_table(aggregated_allocations: List, cloud_cost: float) ->
 
     return distributed_cost
 
+
 def get_cost_cards(cost_table: DataFrame) -> tuple[str]:
     total_sum = "${:.2f}".format(cost_table['TOTAL COST'].sum())
     cloud_sum = "${:.2f}".format(cost_table['CLOUD COST'].sum())
@@ -225,24 +228,26 @@ def get_cost_cards(cost_table: DataFrame) -> tuple[str]:
 
     return total_sum, cloud_sum, compute_sum, storage_sum
 
+
 def buildHistogram(cost_table: DataFrame, bin_by: str):
     top = clean_df(cost_table, bin_by).groupby(bin_by)['TOTAL COST'].sum().nlargest(10).index
     costs = cost_table[cost_table[bin_by].isin(top)]
     data_index = costs.groupby(bin_by)['TOTAL COST'].sum().sort_values(ascending=False).index
     title = "Top " + bin_by.title() + " by Total Cost"
     chart = px.histogram(costs, x='TOTAL COST', y=bin_by, orientation='h',
-                              title=title, labels={bin_by: bin_by.title(), 'TOTAL COST': 'Total Cost'},
-                              hover_data={'TOTAL COST': '$:.2f'},
-                              category_orders={bin_by: data_index})
+                         title=title, labels={bin_by: bin_by.title(), 'TOTAL COST': 'Total Cost'},
+                         hover_data={'TOTAL COST': '$:.2f'},
+                         category_orders={bin_by: data_index})
     chart.update_layout(
         title_text=title,
         title_x=0.5,
-        xaxis_tickprefix = '$',
-        xaxis_tickformat = ',.',
+        xaxis_tickprefix='$',
+        xaxis_tickformat=',.',
         yaxis={  # Trim labels that are larger than 15 chars
             'tickmode': 'array',
             'tickvals': data_index,
-            'ticktext': [f"{txt[:15]}..." if len(txt) > 15 else txt for txt in chart['layout']['yaxis']['categoryarray']]
+            'ticktext': [f"{txt[:15]}..." if len(txt) > 15 else txt for txt in
+                         chart['layout']['yaxis']['categoryarray']]
         },
         dragmode=False
     )
@@ -251,22 +256,24 @@ def buildHistogram(cost_table: DataFrame, bin_by: str):
 
     return chart
 
+
 requests_pathname_prefix = '/{}/{}/r/notebookSession/{}/'.format(
     os.environ.get("DOMINO_PROJECT_OWNER"),
     os.environ.get("DOMINO_PROJECT_NAME"),
     os.environ.get("DOMINO_RUN_ID")
 )
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], routes_pathname_prefix=None, requests_pathname_prefix=requests_pathname_prefix)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], routes_pathname_prefix=None,
+           requests_pathname_prefix=requests_pathname_prefix)
 
 app.layout = html.Div([
-    html.H2('Domino Cost Management Report', style = {'textAlign': 'center', "margin-top": "30px"}),
+    html.H2('Domino Cost Management Report', style={'textAlign': 'center', "margin-top": "30px"}),
     dbc.Row([
         dbc.Col(
-            html.H4('Data select', style = {"margin-top": "20px"}),
+            html.H4('Data select', style={"margin-top": "20px"}),
             width=2
         ),
         dbc.Col(
-            html.Hr(style = {"margin-top": "40px"}),
+            html.Hr(style={"margin-top": "40px"}),
             width=10
         )
     ]),
@@ -278,10 +285,10 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Dropdown(
                 id='time_span_select',
-                options = window_to_param,
-                value = "30d",
-                clearable = False,
-                searchable = False
+                options=window_to_param,
+                value="30d",
+                clearable=False,
+                searchable=False
             ),
             width=2
         ),
@@ -289,11 +296,11 @@ app.layout = html.Div([
     ], style={"margin-top": "30px"}),
     dbc.Row([
         dbc.Col(
-            html.H4('Filter data by', style = {"margin-top": "20px"}),
+            html.H4('Filter data by', style={"margin-top": "20px"}),
             width=2
         ),
         dbc.Col(
-            html.Hr(style = {"margin-top": "40px"}),
+            html.Hr(style={"margin-top": "40px"}),
             width=10
         )
     ], style={"margin-top": "50px"}),
@@ -305,10 +312,10 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Dropdown(
                 id='billing_select',
-                options = ['No data'],
-                clearable = True,
-                searchable = True,
-                style={"width": "100%", "whiteSpace":"nowrap"}
+                options=['No data'],
+                clearable=True,
+                searchable=True,
+                style={"width": "100%", "whiteSpace": "nowrap"}
             ),
             width=3
         ),
@@ -319,10 +326,10 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Dropdown(
                 id='project_select',
-                options = ['No data'],
-                clearable = True,
-                searchable = True,
-                style={"width": "100%", "whiteSpace":"nowrap"}
+                options=['No data'],
+                clearable=True,
+                searchable=True,
+                style={"width": "100%", "whiteSpace": "nowrap"}
             ),
             width=3
         ),
@@ -333,10 +340,10 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Dropdown(
                 id='user_select',
-                options = ['No data'],
-                clearable = True,
-                searchable = True,
-                style={"width": "100%", "whiteSpace":"nowrap"}
+                options=['No data'],
+                clearable=True,
+                searchable=True,
+                style={"width": "100%", "whiteSpace": "nowrap"}
             ),
             width=3
         ),
@@ -370,7 +377,7 @@ app.layout = html.Div([
     dcc.Loading(children=[
         dcc.Graph(
             id='cumulative-daily-costs',
-            config = {
+            config={
                 'displayModeBar': False
             },
             style={'margin-top': '40px'}
@@ -380,7 +387,7 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Graph(
                 id='user_chart',
-                config = {
+                config={
                     'displayModeBar': False
                 }
             )
@@ -388,7 +395,7 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Graph(
                 id='project_chart',
-                config = {
+                config={
                     'displayModeBar': False
                 }
             )
@@ -398,7 +405,7 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Graph(
                 id='org_chart',
-                config = {
+                config={
                     'displayModeBar': False
                 }
             )
@@ -406,7 +413,7 @@ app.layout = html.Div([
         dbc.Col(
             dcc.Graph(
                 id='tag_chart',
-                config = {
+                config={
                     'displayModeBar': False
                 }
             )
@@ -416,6 +423,7 @@ app.layout = html.Div([
     html.Div(id='table-container'),
     dbc.Row([], style={"margin-top": "50px"}),
 ], className="container")
+
 
 def get_dropdown_filters(cost_table: DataFrame) -> tuple:
     """ 
@@ -430,14 +438,17 @@ def get_dropdown_filters(cost_table: DataFrame) -> tuple:
     billing_tags_list.insert(0, NO_TAG)
 
     # For each dropdown data return a dict containing the value/label/title (useful for tooltips)
-    billing_tags_dropdown = [{"label": billing_tag, "value": billing_tag, "title": billing_tag} for billing_tag in billing_tags_list]
+    billing_tags_dropdown = [{"label": billing_tag, "value": billing_tag, "title": billing_tag} for billing_tag in
+                             billing_tags_list]
     projects_dropdown = [{"label": project, "value": project, "title": project} for project in projects_list]
     users_dropdown = [{"label": user, "value": user, "title": user} for user in users_list]
 
     return billing_tags_dropdown, projects_dropdown, users_dropdown
 
+
 def get_cumulative_cost_graph(cost_table: DataFrame, time_span: timedelta):
-    x_date_series = pd.date_range(get_today_timestamp() - get_time_delta(time_span), get_today_timestamp()).strftime('%B %-d')
+    x_date_series = pd.date_range(get_today_timestamp() - get_time_delta(time_span), get_today_timestamp()).strftime(
+        '%B %-d')
     cost_table_grouped_by_date = cost_table.groupby('FORMATTED START')
 
     cumulative_cost_graph = {
@@ -451,11 +462,12 @@ def get_cumulative_cost_graph(cost_table: DataFrame, time_span: timedelta):
         'layout': go.Layout(
             title='Daily Costs by Type',
             barmode='stack',
-            yaxis_tickprefix = '$',
-            yaxis_tickformat = ',.'
+            yaxis_tickprefix='$',
+            yaxis_tickformat=',.'
         )
     }
     return cumulative_cost_graph
+
 
 def get_histogram_charts(cost_table: DataFrame):
     user_chart = buildHistogram(cost_table, 'USER')
@@ -463,6 +475,7 @@ def get_histogram_charts(cost_table: DataFrame):
     org_chart = buildHistogram(cost_table, 'ORGANIZATION')
     tag_chart = buildHistogram(cost_table, 'BILLING TAG')
     return user_chart, project_chart, org_chart, tag_chart
+
 
 def workload_cost_details(cost_table: DataFrame):
     formatted = {'locale': {}, 'nully': '', 'prefix': None, 'specifier': '$,.2f'}
@@ -489,6 +502,7 @@ def workload_cost_details(cost_table: DataFrame):
         }
     )
     return table
+
 
 @app.callback(
     [
@@ -550,6 +564,7 @@ def user_clicked(clickData):
     else:
         return [None]
 
+
 @app.callback(
     [Output('project_select', 'value')],
     [Input('project_chart', 'clickData')]
@@ -560,6 +575,7 @@ def project_clicked(clickData):
         return [x_value]
     else:
         return [None]
+
 
 @app.callback(
     [Output('billing_select', 'value')],
@@ -572,5 +588,6 @@ def billing_tag_clicked(clickData):
     else:
         return [None]
 
+
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0',port=8888) # Domino hosts all apps at 0.0.0.0:8888
+    app.run_server(host='0.0.0.0', port=8888)  # Domino hosts all apps at 0.0.0.0:8888
--- domino_cost/domino_cost.py
+++ domino_cost/domino_cost.py
@@ -2,9 +2,11 @@ import re
 from datetime import timedelta
 import pandas as pd
 from pandas import (
-    # DataFrame,
+    DataFrame,
     Timestamp
 )
+
+from typing import Callable, List
 
 
 def get_domino_namespace(api_host) -> str:
@@ -23,3 +25,52 @@ def get_time_delta(time_span) -> timedelta:
     else:
         days_to_use = int(time_span[:-1])
     return timedelta(days=days_to_use - 1)
+
+
+def process_or_zero(func: Callable, posInt: int) -> int:
+    if posInt > 0:
+        return func
+    else:
+        return 0
+
+
+def distribute_cost(df: DataFrame) -> DataFrame:
+    """
+    distributes __unallocated__ cost for cleaner representation.
+    """
+    fiels_list = ["CPU COST", "GPU COST", "COMPUTE COST", "MEMORY COST", "STORAGE COST", "ALLOC COST"]
+    cost_unallocated = df[df["TYPE"].str.startswith("__")]
+    cost_allocated = df[~df["TYPE"].str.startswith("__")]
+    cost_allocated_total_sum = cost_allocated["ALLOC COST"].sum()
+
+    for field in fiels_list:
+        cost_allocated[field] = cost_allocated[field] + (
+                    (cost_allocated["ALLOC COST"] / cost_allocated_total_sum) * cost_unallocated[field].sum())
+    return cost_allocated
+
+
+def distribute_cloud_cost(df: DataFrame, cost: float) -> DataFrame:
+    """
+    distribute unaccounted cloud cost accross allocated executions.
+    """
+    accounted_cost = df["ALLOC COST"].sum()
+    cloud_cost_unaccounted = process_or_zero(cost - accounted_cost, cost)
+
+    df['CLOUD COST'] = cloud_cost_unaccounted * (df['ALLOC COST'] / accounted_cost)
+    df['TOTAL COST'] = df['ALLOC COST'] + df['CLOUD COST']
+
+    return df
+
+
+def clean_values(values_list: list) -> list:
+    """
+    remove "__unallocated__" from values'
+    """
+    return values_list[1:] if values_list[0].startswith("__") else values_list
+
+
+def clean_df(df: DataFrame, col: str) -> DataFrame:
+    """
+    remove "__unallocated__" records from dataframe for preview.
+    """
+    return df[~df[col].str.startswith("__")]
